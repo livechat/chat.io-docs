2fce9828c711c8a33009571330c7a7ba440fadb9
Filip Jaskolski <f.jaskolski@livechatinc.com>
2017-11-08 14:00:35 +0100

    updated webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 
-mix.js('src/js/app.js', 'dist/docs/js')
+mix.copy('src/_redirects', 'dist/_redirects')
+   .js('src/js/app.js', 'dist/docs/js')
    .sass('src/css/app.scss', 'dist/docs/css')
    .sourceMaps()
    .options({
